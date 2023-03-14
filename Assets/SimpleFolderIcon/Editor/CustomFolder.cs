d123170aa6c8907b3321bbb6cc027cd432c548de
HugoLazaro <801758@unizar.es>
2023-03-14 22:03:09 +0100

    First commit, empty project and .gitignore

---
+++ Assets/SimpleFolderIcon/Editor/CustomFolder.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:104c7901f405850374e6c0d66543320437bee77f395c3e6202afd1eb28882c19
+size 1597
---
+++ Assets/SimpleFolderIcon/Editor/IconDictionaryCreator.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:301618ae6082409c521675a68e22052c79ec344e6c19cd7fe09a4ad618cb9772
+size 1933
