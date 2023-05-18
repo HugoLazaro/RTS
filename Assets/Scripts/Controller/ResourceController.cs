bfb36fbc7b682cdef29c08b3aa03c6c518aa6597
HugoLazaro <hlazarog@hotmail.com>
2023-05-18 13:50:10 +0200

    Ini resources

---
+++ Assets/Scripts/Controller/ResourceController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1d91a130229375445ddca3ec7ae077365bb6c9b019631853a9346863b9238de
+size 320
---
+++ Assets/Scripts/Resource/Resource.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:07f21b96beb59b3296e20188d4a7cd698699f37373c47633deaad6f06937eeb5
+size 310
