f2cb075e33b9820b05deba48a0fdb841c021034c
HugoLazaro <hlazarog@hotmail.com>
2023-05-22 00:56:38 +0200

    modificacion antes de instanciar

--- Assets/Scripts/Building/GridBuildingSystem.cs
+++ Assets/Scripts/Building/GridBuildingSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13b713d46ff2bd9c112702e00746772d364e0f5260b6774d04f187723ed9e80f
-size 5688
+oid sha256:1b5e3aaebd1cea4b23c6f17e96a59346078564dbb100b03dfd7786f25e825f65
+size 5818
