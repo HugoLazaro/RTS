146cb0d5cf8e79ccf6d68e59d13d4f90904dbc7e
HugoLazaro <hlazarog@hotmail.com>
2023-06-18 11:53:22 +0200

    UI recursos

--- Assets/Scripts/UI/ResourceText.cs
+++ Assets/Scripts/UI/ResourceText.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00cfa000ab988f7c408c04a1c5147aaa4623b5312aef4f3fea8a60d87d3320ba
-size 830
+oid sha256:a0c8d5a0d2e9189012505b7fa6b7e464aeeecdb76a86dbae5ec5d56b9769f650
+size 1899
