f6e9d2f11820a94abf4dd6d24c752028f8bf6fc2
HugoLazaro <hlazarog@hotmail.com>
2023-06-23 17:04:24 +0200

    moba minimap

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5755b13edd778deddb464b29495a888a0d59c3f6633533fc342ce642a1fdff3
-size 2171
+oid sha256:f26442fea3e3bfeb2ac3b6ad0429ff306e7d7623a1b73ae99acf5547e6073bfc
+size 2209
---
+++ Assets/Scripts/UI/MiniMapCam.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:99d5568fdbe8d80d0ca3badf36ec75e8d84c5b18b0a8bec13a8f9dc7ecf7feb0
+size 1546
