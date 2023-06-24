70c6b855b30d0d9b39777e8a162121ee25b8a805
HugoLazaro <hlazarog@hotmail.com>
2023-06-24 12:31:55 +0200

    some fix Ui

--- Assets/Scripts/Building/BuildRts.cs
+++ Assets/Scripts/Building/BuildRts.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8070d81b6a08433d02428ab2f52f04ebb5b485e3df2e9790c8ccc641bf9c3126
-size 8196
+oid sha256:64454cc3a8d121668858ff93fadb315e4eb55c179e5d4854925d5f4dada934ea
+size 8409
---
+++ Assets/Scripts/UI/Progress.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:007a48928531e04c491324c2470ca0cc86097753c6d1cd7aade48bfb53af7fc9
+size 740
