cc1aabbc64995526e3c5111b5dbcdae827f801dd
HugoLazaro <hlazarog@hotmail.com>
2023-06-25 20:35:51 +0200

    fixed

--- Assets/Scripts/Resource/ResourceAnim.cs
+++ Assets/Scripts/Resource/ResourceAnim.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23c78aec979c620f7b28ae7f0a3e91a913e6fbdb7fe9ee9c350a02f683d609b9
-size 2594
+oid sha256:9908fd587a78fe0493d2a015ffd2facb61f2a068aff707481960aa194cbc91b1
+size 2630
