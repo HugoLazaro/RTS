ea63afdaebbe4a1f34693bd1b01a44734337fb17
HugoLazaro <hlazarog@hotmail.com>
2023-07-07 18:44:04 +0200

    fix bug sound

--- Assets/Scripts/Controller/MusicController.cs
+++ Assets/Scripts/Controller/MusicController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:595bc185b0e1d4350bbaff4d040fb3871ec198fccb1c274a3b2109c441282c0b
-size 1988
+oid sha256:b47f0b4bab75b44a6e3c39a82fafdc4604e7114d6b30b67c57636642304934fe
+size 2409
--- Assets/Scripts/UI/Ajustes.cs
+++ Assets/Scripts/UI/Ajustes.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6daf467123e8714d430721a889c317bd01dc9b29aaae2ddea20fec0a6a53f6b5
-size 4003
+oid sha256:b21a1968dd950875d615c36c8120c67fa0e56a9d0e3ac4be6d8dd8b167f85f5e
+size 3921
