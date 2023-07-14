39c9c51f5401025dfd85b4a828737e631a42421c
HugoLazaro <hlazarog@hotmail.com>
2023-07-14 19:39:14 +0200

    cambio escena

--- Assets/Scripts/Controller/SceneController.cs
+++ Assets/Scripts/Controller/SceneController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7c7e13eab62a14fc083a91a25a32dcb04043834c5391101ae33d1e14308c513
-size 307
+oid sha256:e657f52516fe26c8108e361d0b510c84482b9c754df414b9a42cffa7d5dcc426
+size 1928
