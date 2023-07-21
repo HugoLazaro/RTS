3dc98a37c53ebc0b5f5e1cff69caaade12d45571
HugoLazaro <hlazarog@hotmail.com>
2023-07-21 16:34:04 +0200

    cambio atack/def por corrutinas

--- Assets/Scripts/IA/IADefensa.cs
+++ Assets/Scripts/IA/IADefensa.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:450d0bbdf67edca86910813aa7799236e5d514fee23fe9e184bbfad95a49f409
-size 17308
+oid sha256:6a6cce68502631f9b5781bf22a8aa8ad186df1d22657306677de92f88602a2e0
+size 18877
--- Assets/Scripts/IA/IAMilitar.cs
+++ Assets/Scripts/IA/IAMilitar.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8df57a5ced54e5c9d31b73b25e141f018100392424184a5a42b7e86bd6a07b9
-size 56998
+oid sha256:dfb5af655fb637636143acea413f93be396ce810228714d5e677039c2204db64
+size 59206
