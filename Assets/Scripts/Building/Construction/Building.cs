0e9b4d072554827b9d5ef03bdd4b873632904a7e
HugoLazaro <hlazarog@hotmail.com>
2023-08-02 11:05:49 +0200

    pantallas de carga de 1 a 0.2s

--- Assets/Scripts/Building/Construction/Building.cs
+++ Assets/Scripts/Building/Construction/Building.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43307a4f247aa5c4493a4455b7f13a879b5258af783ed2dfb80e726f47bc3b7e
-size 6473
+oid sha256:8a378748bcd7b0601433627069204c3dac708d0d2ba36e4805fc14bd0e4d0b8c
+size 6627
