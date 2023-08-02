75d84ded4b87af4cfd0f5af71df9157d429268d2
HugoLazaro <hlazarog@hotmail.com>
2023-08-02 11:39:22 +0200

    bug poblacion

--- Assets/Scripts/Building/BuildRts.cs
+++ Assets/Scripts/Building/BuildRts.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b440644fb6f8b2572058afc39588cca524df2fb3bdd6e5f002dd42dcf3658346
-size 9932
+oid sha256:994a8d0c0e3e95fc77519f9135cf9c0158698b3ad4b1bf6e2e6b25cfe633e07c
+size 9982
