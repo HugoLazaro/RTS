435b01be7f8c67b03fc20d5f3bd97babd367ac6d
HugoLazaro <hlazarog@hotmail.com>
2023-08-02 10:10:51 +0200

    Españolizar texto + area de seleccion incrementada

--- Assets/Scripts/Controller/CursorController.cs
+++ Assets/Scripts/Controller/CursorController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f92a94814e1058580bc5d9a944d2807bbb4ad9202ea31bfa58689729f32d5709
-size 7578
+oid sha256:50286cd9fab3c80d692349143ddfbf16d3ab5f92f519b45cf66167b37870028c
+size 7579
--- Assets/Scripts/Controller/DamageableController.cs
+++ Assets/Scripts/Controller/DamageableController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:483401f1b0f2f224dcd56d5e18e5a3e6a4d59581a066eb322b6766867108ca63
-size 14701
+oid sha256:99bd64cf8d63df2a9f35b2ea5beca736f7131f82f82e89aca9562d78f1b8f1da
+size 14702
