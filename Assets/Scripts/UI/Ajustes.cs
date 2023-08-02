1723ff6b1c3adda6995f247f256b5e727c878252
HugoLazaro <hlazarog@hotmail.com>
2023-08-02 10:59:44 +0200

    cambio en texto/nombre de modos de juego

--- Assets/Scripts/UI/Ajustes.cs
+++ Assets/Scripts/UI/Ajustes.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:426a1dc30ff674de321a133142c351cc2ae6f4c0cd48021419b7dd48cae511f6
-size 4418
+oid sha256:cedacac4d2f295390dbae52140fec78c9340e1472b4dd1b9e77755f00374368c
+size 4482
