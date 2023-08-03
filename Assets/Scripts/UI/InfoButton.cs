42258e0490974b9731fc85c0f35763dbddcd0339
HugoLazaro <hlazarog@hotmail.com>
2023-08-03 16:12:21 +0200

    Ui aviso de daño por terreno

--- Assets/Scripts/UI/InfoButton.cs
+++ Assets/Scripts/UI/InfoButton.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93a7a3d2ff2591a275859689e5b365666ae1dd023f7955303762020d080e7ac2
-size 3017
+oid sha256:86fff2ac3b8a235ea7241079eadd3b53eeaa8112a06beea5c16bf97636b1226b
+size 2942
