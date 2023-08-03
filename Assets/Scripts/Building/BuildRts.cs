971b0f5053415dcc0097a5530f698b4602916b4e
HugoLazaro <hlazarog@hotmail.com>
2023-08-03 10:10:54 +0200

    seleccion de unit/build mas clara

--- Assets/Scripts/Building/BuildRts.cs
+++ Assets/Scripts/Building/BuildRts.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:994a8d0c0e3e95fc77519f9135cf9c0158698b3ad4b1bf6e2e6b25cfe633e07c
-size 9982
+oid sha256:8656f75b4b22a8d40e3d5e710326310c9c08f6db101ceec8928087bda3f43a1d
+size 10425
--- Assets/Scripts/Units/UnitRts.cs
+++ Assets/Scripts/Units/UnitRts.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:386966ef858f09e6371ec6ba226f7ee5faf5b52b496c4190bcbf13b25b05cce5
-size 19493
+oid sha256:7bb5fcdadba22d6d90e03cfdec0e882750d2b0d6513245cb014a7b042518da97
+size 19789
