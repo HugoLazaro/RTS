c291ec85ee7dc8d1e1024b859686e84259b8a1cf
HugoLazaro <hlazarog@hotmail.com>
2023-08-03 15:51:26 +0200

    Ui de los nuevos cambios a los modos de juego

--- Assets/Scripts/Controller/ModosAspectController.cs
+++ Assets/Scripts/Controller/ModosAspectController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afe21d6d70975d2b9d80628d0a72603f76703d0031fecf72d73459ffe58959b3
-size 4635
+oid sha256:dbf1993d13a86ef1beaf49030b184eca4ca8a6bdbe407ab42d004b852d861e2a
+size 5743
