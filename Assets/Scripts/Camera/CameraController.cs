ec145586df1b197494679bbba3456a256d549c19
HugoLazaro <hlazarog@hotmail.com>
2023-08-03 17:09:14 +0200

    Aumneto limites de cam segun size

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:392a727d268235c702ccc7731b25c6324640adec22b375da6e5150f3c334125b
-size 5297
+oid sha256:8fef9e2b100eb957661b1e64b975f0a30db24cc5da5a6e451f4eaab5ed9637a9
+size 5410
