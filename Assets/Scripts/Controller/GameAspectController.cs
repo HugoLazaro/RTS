29f50dbb3b7516bfa5c5327e35bd281b60f107c5
HugoLazaro <hlazarog@hotmail.com>
2023-08-03 12:22:13 +0200

    Ui confirmacion salida

--- Assets/Scripts/Controller/GameAspectController.cs
+++ Assets/Scripts/Controller/GameAspectController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66a9054664b3828a17a11f1838e2522affede1f24bf81f67fcacf0af9bff7bbf
-size 4790
+oid sha256:1d533e43c2c222e4c03dd19355607d0b740600f75d773553bcf7c94450caa0bb
+size 5056
--- Assets/Scripts/Controller/MenuAspectController.cs
+++ Assets/Scripts/Controller/MenuAspectController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea24cf29f7f06fa9794cbb2abdaa3760c7628de195b4828ece49e62f464465a8
-size 3978
+oid sha256:560ca3104e396cf93b1478ceb57098a1bff141721a6e657c06417f62c79c4eaa
+size 4728
